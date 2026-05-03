4e357bef580cc47ab1a8419442ba8bae42f89159
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-03 19:16:03 +0530

    Add decrement and get_counter endpoints in main.rs
    - Implemented a new `decrement` endpoint to decrease the shared counter, ensuring it does not go below zero.
    - Added a `get_counter` endpoint to retrieve the current value of the counter.
    - Updated the application routing to include these new endpoints, enhancing the counter functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::Mutex;
 
 use axum::{Json, Router, extract::State, routing::get};
-use serde::de::value::Error;
 
 struct AppState {
     counter: Mutex<u32>,
 }
 
 async fn increment(State(data): State<Arc<AppState>>) -> Json<u32> {
-    let mut state = data.counter.lock().unwrap();
+    let mut state = data.counter.lock().await;
     *state += 1;
 
+    Json(*state)
+}
+
+async fn decrement(State(data): State<Arc<AppState>>) -> Json<u32> {
+    let mut state = data.counter.lock().await;
+
+    if *state <= 0 {
+        *state = 0;
+    } else {
+        *state -= 1;
+    }
+
+    Json(*state)
+}
+
+async fn get_counter(State(data): State<Arc<AppState>>) -> Json<u32> {
+    let state = data.counter.lock().await;
     Json(*state)
 }
 
@@ -22,6 +39,8 @@ async fn main() {
     });
     let app = Router::new()
         .route("/increment", get(increment))
+        .route("/decrement", get(decrement))
+        .route("/counter", get(get_counter))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
