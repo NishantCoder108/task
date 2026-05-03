403278b4d848d61a1b77f11498648091457b070d
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-03 19:02:48 +0530

    Refactor main.rs to implement a counter endpoint and simplify routing
    - Removed unused routes and data structures related to user creation and item listing.
    - Introduced a new `increment` endpoint that increments a shared counter using `Arc` and `Mutex`.
    - Updated the application state management to utilize Axum's `State` for shared data access.

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,37 @@
-use axum::{
-    Json, Router,
-    extract::{Path, Query},
-    http::Uri,
-    routing::{get, post},
-    serve,
-};
-use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 
-async fn home() -> String {
-    "Home url".to_string()
+use axum::{Json, Router, extract::State, routing::get};
+use serde::de::value::Error;
+
+struct AppState {
+    counter: Mutex<u32>,
 }
 
-#[derive(Deserialize)]
-struct QuestionItem {
-    question_id: u64,
-    comment_id: u64,
-}
+async fn increment(State(data): State<Arc<AppState>>) -> Json<u32> {
+    let mut state = data.counter.lock().unwrap();
+    *state += 1;
 
-#[derive(Deserialize, Serialize)]
-struct CreateUser {
-    username: String,
-    firstname: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct ResponseUser {
-    username: String,
-    message: String,
-}
-
-// localhost:3000/items?page=2&per_page=30
-
-#[derive(Deserialize, Serialize)]
-struct Pagination {
-    page: Option<u64>,
-    per_page: Option<u64>,
-}
-
-#[derive(Deserialize, Serialize)]
-struct ResponseItems {
-    message: String,
-    page: u64,
-    per_page: u64,
-}
-
-async fn list_items(Query(pagination): Query<Pagination>) -> Json<ResponseItems> {
-    // format!(
-    //     "Page {} have {} items.",
-    //     pagination.page.unwrap_or(1),
-    //     pagination.per_page.unwrap_or(10)
-    // )
-
-    let res = ResponseItems {
-        message: "Hey, List items retrived succesfully.".to_string(),
-        per_page: pagination.per_page.unwrap_or(20),
-        page: pagination.page.unwrap_or(1),
-    };
-
-    Json(res)
-}
-
-// async fn get_question(Path((a, b)): Path<(u64, u64)>) -> String {
-//     format!("Question Id : {b}, Comment Id: {a}")
-// }
-
-async fn get_question(Path(params): Path<QuestionItem>) -> String {
-    format!(
-        "Question Id: {} , Comment Id: {}",
-        params.question_id, params.comment_id
-    )
-}
-
-async fn create_user(Json(user): Json<CreateUser>) -> Json<ResponseUser> {
-    // format!(
-    //     "Created User, Firstname: {}, Username: {}",
-    //     user.firstname, user.username
-    // )
-
-    let user = ResponseUser {
-        message: "User created successfully".to_string(),
-        username: user.username,
-    };
-
-    Json(user)
+    Json(*state)
 }
 
 #[tokio::main]
+
 async fn main() {
+    let state = Arc::new(AppState {
+        counter: Mutex::new(0),
+    });
     let app = Router::new()
-        .route("/", get(home))
-        .route(
-            "/question/{question_id}/comment/{comment_id}",
-            get(get_question),
-        )
-        .route("/create_user", post(create_user))
-        .route("/items", get(list_items));
-
-    let url = "http://localhost:3000/items?page=3&per_page=345"
-        .parse()
-        .unwrap();
-    let res: Query<Pagination> = Query::try_from_uri(&url).unwrap();
-
-    println!(
-        "Page : {}, Per Page : {}",
-        res.page.unwrap(),
-        res.per_page.unwrap()
-    );
+        .route("/increment", get(increment))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
+
+/*
+Mutex: Only one thread access the data at a time
+Arc: Atomic reference counter, It help to share the ownership of same data
+State:
+
+*/
